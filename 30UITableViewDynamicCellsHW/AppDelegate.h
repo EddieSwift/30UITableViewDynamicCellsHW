bfe89cecfacb8ad4ab47b07713108ffdc83023ac
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2019-01-27 10:15:51 +0200

    Initial Commit

---
+++ 30UITableViewDynamicCellsHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  30UITableViewDynamicCellsHW
+//
+//  Created by Eduard Galchenko on 1/27/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 30UITableViewDynamicCellsHW/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  30UITableViewDynamicCellsHW
+//
+//  Created by Eduard Galchenko on 1/27/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
