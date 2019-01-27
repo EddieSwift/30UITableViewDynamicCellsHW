a5d3bdb864afbdf19f28651aa8b4adae3cec03ad
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2019-01-27 21:34:42 +0200

    Level "learner"

--- 30UITableViewDynamicCellsHW/ViewController.m
+++ 30UITableViewDynamicCellsHW/ViewController.m
@@ -10,13 +10,83 @@
 
 @interface ViewController ()
 
+@property (assign, nonatomic) CGFloat red;
+@property (assign, nonatomic) CGFloat green;
+@property (assign, nonatomic) CGFloat blue;
+
+@property (strong, nonatomic) NSString *rgbColorData;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    NSLog(@"numberOfSectionsInTableView - %@", tableView);
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    NSLog(@"numberOfRowsInSection %ld", (long)section);
+    
+    return 1000;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSLog(@"cellForRowAtIndexPath {%ld, %ld}", (long)indexPath.section, (long)indexPath.row);
+    
+    static NSString *identifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        NSLog(@"Cell created!");
+        
+    } else {
+        
+        NSLog(@"Cell reused!");
+    }
+    
+    NSString *rgbText = [NSString stringWithFormat:@"RGB(%1.0f, %1.0f, %1.0f)", self.red, self.green, self.blue];
+    
+    cell.textLabel.text = rgbText;
+    
+    UIColor *rgbRandomColor = [self randomColor];
+    
+    cell.backgroundColor = rgbRandomColor;
+    
+    return cell;
+}
+
+#pragma mark - Private methods
+
+- (UIColor*) randomColor {
+    
+    NSLog(@"New color for cell created!");
+    
+    CGFloat r = (CGFloat)(arc4random() % 256) / 255.f;
+    CGFloat g = (CGFloat)(arc4random() % 256) / 255.f;
+    CGFloat b = (CGFloat)(arc4random() % 256) / 255.f;
+    
+    self.red = (CGFloat)(r * 255.f);
+    self.green = (CGFloat)(g * 255.f);
+    self.blue = (CGFloat)(b * 255.f);
+
+    NSLog(@"RGB(%f, %f, %f)", r, g, b);
+    
+    return [UIColor colorWithRed:r green:g blue:b alpha:1.f];
 }
 
 
